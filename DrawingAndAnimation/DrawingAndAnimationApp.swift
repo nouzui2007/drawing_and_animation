71b720982e8243be8591a7a95fdb44f408d3bfc7
Hotaka Makiuchi <makiuchi@ccube.jp>
2023-09-19 12:25:55 +0900

    Initial Commit

---
+++ DrawingAndAnimation/DrawingAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAndAnimationApp.swift
+//  DrawingAndAnimation
+//
+//  Created by 牧内 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
